fb68d536640481b03512afc69bf426ecba81917e
Artem <artem_starchenko_20025@mail.ru>
2024-08-22 12:46:20 +0300

    initial commit Add project files.

---
+++ GitHubUserActivityApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using GitHubUserActivityApp;
+
+Console.Write("Write your username: ");
+var username = Console.ReadLine();
+if (username.Length == 0)
+{
+    Console.WriteLine("Usage: github-activity <username>");
+    return;
+}
+
+GitHubApiWorker gitHubApiWorker = new GitHubApiWorker(username);
+await gitHubApiWorker.UserDataReturnerAsync();
+Console.ReadLine();
+
